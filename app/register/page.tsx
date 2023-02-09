747a78a8117bdd9b61e125d4c3e04111832d4858
naekit <mykyta.onishchuck@code.berlin>
2023-02-08 23:46:41 -0500

    added redirecting for registration

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { useState } from "react"
+import { signIn } from "next-auth/react"
 
 const Register = () => {
 	const [email, setEmail] = useState("")
@@ -24,7 +25,13 @@ const Register = () => {
 			)
 			if (!res.ok) console.log(res)
 			const data = await res.json()
-			console.log(data)
+			const signInData = await signIn("credentials", {
+				callbackUrl: "/",
+				redirect: true,
+				email,
+				password,
+			})
+			console.log(signInData, data)
 		} catch (error) {
 			console.log(error)
 		}
@@ -38,18 +45,21 @@ const Register = () => {
 			>
 				<h1 className="text-4xl font-bold mb-4">Register</h1>
 				<input
+					placeholder="Enter Name"
 					type="text"
 					value={name}
 					onChange={(e) => setName(e.target.value)}
 					className="mt-4 py-3 w-96 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 				/>
 				<input
+					placeholder="Enter Email"
 					type="email"
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 					className="mt-4 py-3 w-96 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 				/>
 				<input
+					placeholder="Enter Password"
 					type="password"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
