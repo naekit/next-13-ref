b653a8e25d0ec35694f9344512debb6335f362cd
naekit <mykyta.onishchuck@code.berlin>
2023-02-09 19:02:56 -0500

    further nav

--- app/NavLink.tsx
+++ app/NavLink.tsx
@@ -12,8 +12,8 @@ export default function NavLink({ label, path, targetSegment }: any) {
 			href={path}
 			className={`${
 				isActive
-					? "text-zinc-50 font-bold bg-orange-400 p-2 rounded-tr-sm rounded-tl-sm"
-					: "text-zinc-900 font-bold p-2"
+					? "text-zinc-50 hover:text-zinc-900 font-bold bg-orange-400 p-2 rounded-tr-sm rounded-tl-sm"
+					: "text-zinc-900 font-bold p-2 hover:text-orange-600"
 			}`}
 		>
 			{label}
